b343a40d7457a22d0fcbfb564b13813469bfdfe0
Samuel Herts <hertssd@ornl.gov>
2026-02-26 14:59:37 -0500

    Fix comment

--- src/client/auditclient/auditclient_aarch64.c
+++ src/client/auditclient/auditclient_aarch64.c
@@ -29,7 +29,7 @@ Place, Suite 330, Boston, MA 02111-1307 USA
                                     La_aarch64_regs *regs,
                                     unsigned int *flags,
                                     const char *symname,
-                                    long int *framesizep) AUDIT_EXPORT;
+                                    long int *framesizep) AUDIT_EXPORT;*/
 
 Elf64_Addr la_aarch64_gnu_pltenter(Elf64_Sym *sym, unsigned int ndx,
                                     uintptr_t *refcook,
